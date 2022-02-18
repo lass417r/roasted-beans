9faac6f85b4cf02a1bde3fca9a1efeaaf0421870
”Lasse <lass417r@stud.kea.dk>
2022-02-18 10:42:31 +0100

    Roasted bean v1

---
+++ roastedbean/js/javaScript_index.js
@@ -0,0 +1,85 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const url = "https://passionproject-5d13.restdb.io/rest/passionproject";
+const key = "620e15ef34fd6215658586f3";
+const options = {
+  headers: {
+    "x-apikey": key,
+  },
+};
+
+let beans;
+let filter = "alle";
+const jpg = ".jpg";
+
+function start() {
+  const filterKnapper = document.querySelectorAll("nav button");
+  filterKnapper.forEach((knap) =>
+    knap.addEventListener("click", filtrerKategori)
+  );
+  loadJSON();
+}
+
+function filtrerKategori() {
+  filter = this.dataset.land;
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+
+  visBeans();
+
+  const txtKategori = document.querySelector("header .txt_kategori");
+  console.log(this);
+  txtKategori.textContent = "Filter: " + this.textContent;
+}
+
+async function loadJSON() {
+  const JSONData = await fetch(url, options);
+  beans = await JSONData.json();
+  console.log(beans);
+  visBeans();
+}
+
+//grid view
+function visBeans() {
+  const container = document.querySelector("section");
+  const temp = document.querySelector("template");
+  container.textContent = "";
+
+  beans.forEach((bean) => {
+    if (filter == bean.land || filter == "alle") {
+      let klon = temp.cloneNode(true).content;
+      klon.querySelector(".navn").textContent = bean.navn;
+      klon.querySelector(".billede").src = "medium/" + bean.billed_id + jpg;
+      klon.querySelector(".smagsnoter").textContent = bean.smagsnoter;
+      klon.querySelector(".land").textContent = bean.land;
+      klon.querySelector(".pris").textContent = "Pris: " + bean.pris + " kr.-";
+      klon
+        .querySelector("article")
+        .addEventListener("click", () => visDetaljer(bean));
+      container.appendChild(klon);
+    }
+  });
+}
+
+document.querySelector("#popup button").addEventListener("click", lukPopup);
+
+function lukPopup() {
+  document.querySelector("#popup").style.display = "none";
+}
+//popup view
+function visDetaljer(bean) {
+  console.log(bean);
+  const popup = document.querySelector("#popup");
+  popup.style.display = "block";
+  popup.querySelector(".navn").textContent = bean.navn;
+  popup.querySelector(".billede").src = "medium/" + bean.billed_id + jpg;
+  popup.querySelector(".kortbeskrivelse").textContent = bean.beskrivelse_kort;
+  popup.querySelector(".smagsnoter").textContent = bean.smagsnoter;
+  popup.querySelector(".land").textContent = bean.land;
+  popup.querySelector(".pris").textContent = bean.pris + " kr.-";
+  popup.querySelector(".videre").addEventListener("click", () => {
+    location.href = "single_view.html?id=" + bean._id;
+  });
+}
---
+++ roastedbean/js/javaScript_single_view.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const url = "https://passionproject-5d13.restdb.io/rest/passionproject/";
+const key = "620e15ef34fd6215658586f3";
+const options = {
+  headers: {
+    "x-apikey": key,
+  },
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+let bean;
+
+document.addEventListener("DOMContentLoaded", loadJSON);
+
+async function loadJSON() {
+  const JSONData = await fetch(url + id, options);
+  bean = await JSONData.json();
+  console.log(bean);
+  visBeans(bean);
+}
+
+function visBeans(bean) {
+  const jpg = ".jpg";
+  document.querySelector(".navn").textContent = bean.navn;
+  document.querySelector(".billede").src = "medium/" + bean.billed_id + jpg;
+  document.querySelector(".langbeskrivelse").textContent =
+    bean.beskrivelse_lang;
+  document.querySelector(".smagsnoter").textContent = bean.smagsnoter;
+  document.querySelector(".land").textContent = bean.land;
+  document.querySelector(".proccess").textContent = bean.proccess;
+  document.querySelector(".velegnet").textContent = bean.velegnet_til;
+  document.querySelector(".pris").textContent = bean.pris + " kr.-";
+  document.querySelector("button").addEventListener("click", () => {
+    window.history.back();
+  });
+}
